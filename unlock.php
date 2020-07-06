e082c3a2be07f66d6f285032b37d100431dc2498
iVampireSP.com <im@ivampiresp.com>
2020-07-06 09:25:03 +0800

    Push时忘了点什么。。。

--- unlock.php
+++ unlock.php
@@ -17,7 +17,7 @@ require_once('class/User.class.php');
     <?php mduiBody(); mduiHeader('解锁') ; mduiMenu(); ?>
     <form name="unlock" method="post" action="unlock.php">
         <div class="mdui-textfield mdui-textfield-floating-label">
-            <input autocomplete="off" style="text-align: center" class="mdui-textfield-input" type="password" name="password" placeholder="要解锁，请插入密钥并触摸" autofocus required />
+            <input autocomplete="off" style="text-align: center" class="mdui-textfield-input" type="password" name="password" placeholder="要解锁，请输入配置文件中的密码。" autofocus required />
         </div>
         <input name="Submit" type="submit" style="display: none;" value="解锁" />
     </form>
